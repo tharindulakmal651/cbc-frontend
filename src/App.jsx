66a83a9707e81d5e6abbd567a8677a08d16b89b3
tharindulakmal651 <155797706+tharindulakmal651@users.noreply.github.com>
2026-04-04 09:49:20 +0530

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
 import './App.css';
-import ProductCard from './components/ProductCard';
-import UserData from './components/UserData';
-import Testing from './components/testing';
-import  LoginPage from './pages/loginPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/loginPage';
 import Home from './pages/homePage';
+
 function App() {
   return (
-    <>
-   <LoginPage />
-   <Home /> 
-     
-      
-    </>
+
+    
+    <BrowserRouter>
+      <Routes path="/*">
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+
+      </Routes>
+    </BrowserRouter>
+
+    
   );
 }
 
 export default App;
-
-
-/*  <ProductCard  name="Laptop" src = "https://th.bing.com/th/id/OIP.TmdVZty6FfKab0vUeFwvaAHaEm?w=271&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" price ="$499.99"/>
-      <ProductCard  name="IPhone 16 pro" src = "https://th.bing.com/th/id/OIP.PW67lLWsPetRy8KbPT4GTgHaE8?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3" price ="$299.99"/>
-      <ProductCard  name="HeadPhones" src = "https://tse4.mm.bing.net/th/id/OIP.L24QJliF_qStVX34REHgdwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" price ="$49.99" />
-      <ProductCard  name="Smart Watch" src = "https://th.bing.com/th/id/OIP.wvQ-AEBLdLyMJcUgvXu2cwHaE8?w=262&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" price ="$199.99"/>
-      <UserData />
-
-       <Testing />
-
-      */
